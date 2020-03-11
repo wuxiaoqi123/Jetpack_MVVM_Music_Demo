e7a149e92574135f72636cb9d8f02e68f0db1677
wuxiaoqi <1321972760@qq.com>
2020-03-11 15:18:58 +0800

    bindingAdapter

---
+++ app/src/main/java/com/example/music/base/AdapterBinding.java
@@ -0,0 +1,23 @@
+package com.example.music.base;
+
+import androidx.core.view.GravityCompat;
+import androidx.databinding.BindingAdapter;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class AdapterBinding {
+
+    @BindingAdapter(value = {"isOpenDrawer"}, requireAll = false)
+    public static void openDrawer(DrawerLayout drawerLayout, boolean isOpenDrawer) {
+        if (isOpenDrawer && !drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        } else {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @BindingAdapter(value = {"allowDrawerOpen"}, requireAll = false)
+    public static void allowDrawerOpen(DrawerLayout drawerLayout, boolean allowDrawerOpen) {
+        drawerLayout.setDrawerLockMode(allowDrawerOpen ? DrawerLayout.LOCK_MODE_UNLOCKED :
+                DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+    }
+}
