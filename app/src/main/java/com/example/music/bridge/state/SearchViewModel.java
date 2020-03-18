37cdf124c4bb140e4c79d1804715fbe3325724fd
wuxiaoqi <1321972760@qq.com>
2020-03-18 09:55:24 +0800

    fragment_search

---
+++ app/src/main/java/com/example/music/bridge/state/SearchViewModel.java
@@ -0,0 +1,6 @@
+package com.example.music.bridge.state;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+}
--- app/src/main/java/com/example/music/ui/page/SearchFragment.java
+++ app/src/main/java/com/example/music/ui/page/SearchFragment.java
@@ -10,18 +10,44 @@ import androidx.annotation.Nullable;
 
 import com.example.music.R;
 import com.example.music.base.BaseFragment;
+import com.example.music.bridge.state.SearchViewModel;
+import com.example.music.databinding.FragmentSearchBinding;
+import com.example.music.ui.helper.DrawerCoordinateHelper;
 
 public class SearchFragment extends BaseFragment {
+
+    private FragmentSearchBinding mBinding;
+    private SearchViewModel mSearchViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSearchViewModel = getFragmentViewModelProvider(this).get(SearchViewModel.class);
+        getLifecycle().addObserver(DrawerCoordinateHelper.getInstance());
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mBinding = FragmentSearchBinding.bind(view);
+        mBinding.setClick(new ClickProxy());
+        mBinding.setVm(mSearchViewModel);
         return view;
+    }
+
+    public class ClickProxy {
+
+        public void back() {
+            nav().navigateUp();
+        }
+
+        public void testNav() {
+
+        }
+
+        public void subscribe() {
+
+        }
     }
 }
